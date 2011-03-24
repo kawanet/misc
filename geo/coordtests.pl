34d657d37f5e8bf4475cc0a31ec7495bcad58a16
Yusuke Kawasaki <u-suke@kawa.net>
2011-03-24 14:39:37 +0900

    lat long order

--- geo/coordtests.pl
+++ geo/coordtests.pl
@@ -6,20 +6,19 @@ use warnings;
 # Geo::Proj4 by markov
 
 use Geo::Proj4;
-use Geo::Point;
-my $wgs84   = Geo::Proj4->new("+proj=longlat +ellps=WGS84  +datum=WGS84");
-my $towgs84 = Geo::Proj4->new("+proj=longlat +ellps=bessel +towgs84=-146.336,506.832,680.254");
+my $wgs84 = Geo::Proj4->new("+proj=longlat +ellps=WGS84  +datum=WGS84");
+my $tokyo = Geo::Proj4->new("+proj=longlat +ellps=bessel +towgs84=-146.336,506.832,680.254");
 
 sub markov_tokyo_to_wgs84 {
-	my ($long, $lat) = @_;
-	my $point = $towgs84->transform($wgs84, [$long, $lat]);
-	($point->[0], $point->[1]);
+	my ($lat, $long) = @_;
+	my $point = $tokyo->transform($wgs84, [$long, $lat]);
+	($point->[1], $point->[0]);
 }
 
 sub markov_wgs84_to_tokyo {
-	my ($long, $lat) = @_;
-	my $point = $wgs84->transform($towgs84, [$long, $lat]);
-	($point->[0], $point->[1]);
+	my ($lat, $long) = @_;
+	my $point = $wgs84->transform($tokyo, [$long, $lat]);
+	($point->[1], $point->[0]);
 }
 
 # Geo::Coordinates::Converter by yappo
@@ -27,17 +26,17 @@ sub markov_wgs84_to_tokyo {
 use Geo::Coordinates::Converter;
 
 sub yappo_wgs84_to_tokyo {
-	my ($long, $lat) = @_;
+	my ($lat, $long) = @_;
 	my $geo = Geo::Coordinates::Converter->new(lat => $lat, lng => $long, datum => 'wgs84');
 	my $point = $geo->convert(degree => 'tokyo');
-	($point->lng, $point->lat);
+	($point->lat, $point->lng);
 }
 
 sub yappo_tokyo_to_wgs84 {
-	my ($long, $lat) = @_;
+	my ($lat, $long) = @_;
 	my $geo = Geo::Coordinates::Converter->new(lat => $lat, lng => $long, datum => 'tokyo');
 	my $point = $geo->convert(degree => 'wgs84');
-	($point->lng, $point->lat);
+	($point->lat, $point->lng);
 }
 
 # Location::GeoTool by kokogiko
@@ -45,26 +44,26 @@ sub yappo_tokyo_to_wgs84 {
 use Location::GeoTool;
 
 sub kokogiko_tokyo_to_wgs84 {
-	my ($long, $lat) = @_;
+	my ($lat, $long) = @_;
 	my $geo = Location::GeoTool->create_coord($lat, $long, 'tokyo', 'degree')->datum_wgs84;
-	($geo->long, $geo->lat);
+	($geo->lat, $geo->long);
 }
 
 sub kokogiko_wgs84_to_tokyo {
-	my ($long, $lat) = @_;
+	my ($lat, $long) = @_;
 	my $geo = Location::GeoTool->create_coord($lat, $long, 'wgs84', 'degree')->datum_tokyo;
-	($geo->long, $geo->lat);
+	($geo->lat, $geo->long);
 }
 
 # tokyo -> wgs84 -> tokyo tests
 
-my @sapporo = (141.354209, 43.056587);
-my @tokyo   = (139.738293, 35.628538);
-my @naha    = (127.699931, 26.221828);
+my @hokkaido = (155023450/3600000, 508861960/3600000);
+my @tokyo    = (128470740/3600000, 502902170/3600000);
+my @okinawa  = ( 94350640/3600000, 459658600/3600000);
 
-roundtrip('sapporo', @sapporo);
-roundtrip('tokyo',   @tokyo);
-roundtrip('naha',    @naha);
+roundtrip('hokkaido', @hokkaido);
+roundtrip('tokyo',    @tokyo);
+roundtrip('okinawa',  @okinawa);
 
 sub roundtrip {
 	my $title  = shift;
@@ -78,9 +77,9 @@ sub roundtrip {
 	my @yappoT    = yappo_wgs84_to_tokyo(@yappoW);
 	my @markovT   = markov_wgs84_to_tokyo(@markovW);
 
-	printf("source:    place: %-10s            tokyo: %10.6f %9.6f\n", $title,     @source);
-	printf("kokogiko:  wgs84: %10.6f %9.6f  tokyo: %10.6f %9.6f\n",    @kokogikoW, @kokogikoT);
-	printf("yappo:     wgs84: %10.6f %9.6f  tokyo: %10.6f %9.6f\n",    @yappoW,    @yappoT);
-	printf("markov:    wgs84: %10.6f %9.6f  tokyo: %10.6f %9.6f\n",    @markovW,   @markovT);
+	printf("source:    place: %-10s             tokyo: %10.6f %9.6f\n", $title,     @source);
+	printf("kokogiko:  wgs84: %10.6f %9.6f  tokyo: %10.6f %9.6f\n",     @kokogikoW, @kokogikoT);
+	printf("yappo:     wgs84: %10.6f %9.6f  tokyo: %10.6f %9.6f\n",     @yappoW,    @yappoT);
+	printf("markov:    wgs84: %10.6f %9.6f  tokyo: %10.6f %9.6f\n",     @markovW,   @markovT);
 	printf("\n");
 }
